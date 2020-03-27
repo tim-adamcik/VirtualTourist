60f9facf6b7d85fffa9a394a8b0d0e2e5e6dd782
Timothy Adamcik <tadamcik0425@gmail.com>
2020-03-27 12:47:16 -0400

    collection view adjustments

--- VirtualTourist/Controller/PhotoAlbumViewController.swift
+++ VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -21,12 +21,19 @@ class PhotoAlbumViewController: UIViewController, UICollectionViewDelegate, UICo
     
     var currentLatitude: Double?
     var currentLongitude: Double?
-    let spacingBetweenItems:CGFloat = 5
+    let spacingBetweenItems:CGFloat = 3
     var pin: Pin!
     var flickrPhotos: [URL]?
     var savedImages:  [Photo] = []
-    
-    
+    let saveBtn = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: Selector(("saveBtnPressed")))
+    let cancelBtn = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: Selector(("cancelBtnPressed")))
+    private let sectionInsets = UIEdgeInsets(top: 50.0,
+                                             left: 20.0,
+                                             bottom: 50.0,
+                                             right: 20.0)
+    private let itemsPerRow: CGFloat = 3
+
+
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -41,31 +48,34 @@ class PhotoAlbumViewController: UIViewController, UICollectionViewDelegate, UICo
                 cell.activityIndicator.startAnimating()
                 if let dataForImage =  try? Data(contentsOf: desiredArray[indexPath.row]) {
                     cell.photoImage.image = UIImage(data: dataForImage)
-                    cell.activityIndicator.stopAnimating()
                 } else {
                     cell.photoImage.image = #imageLiteral(resourceName: "film-reel")
                 }
+                cell.activityIndicator.stopAnimating()
             }
         }
         return cell
     }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let width = UIScreen.main.bounds.width / 3 - spacingBetweenItems
-        let height = width
-        return CGSize(width: width, height: height)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        
-        return spacingBetweenItems
-    }
-    
+
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//        let width = UIScreen.main.bounds.width / 3 - spacingBetweenItems
+//        let height = width
+//        return CGSize(width: width, height: height)
+//    }
+//
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//
+//        return spacingBetweenItems
+//    }
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.rightBarButtonItems = [saveBtn,cancelBtn]
+        cancelBtn.isEnabled = false
         smallMapView.delegate = self
         collectionView.delegate = self
         collectionView.dataSource = self
@@ -87,14 +97,23 @@ class PhotoAlbumViewController: UIViewController, UICollectionViewDelegate, UICo
                 }
             }
         }
-        
-        // Flow layout
-        let space: CGFloat = 3.0
-        let dimension = (self.view.frame.size.width - (2 * space)) / 3.0
-        
-        flowLayout.minimumInteritemSpacing = spacingBetweenItems
-        flowLayout.minimumLineSpacing = spacingBetweenItems
-        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
+
+//         Flow layout
+//        let space: CGFloat = 3.0
+//        let dimension = (self.view.frame.size.width - (2 * space)) / 3.0
+//
+//        flowLayout.minimumInteritemSpacing = spacingBetweenItems
+//        flowLayout.minimumLineSpacing = spacingBetweenItems
+//        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
+    }
+    
+    @objc func saveBtnPressed(sender: UIBarButtonItem) {
+        saveBtn.isEnabled = false
+        cancelBtn.isEnabled = true
+    }
+    @objc func cancelBtnPressed(sender: UIBarButtonItem) {
+        saveBtn.isEnabled = true
+        cancelBtn.isEnabled = false
     }
     
     
@@ -118,10 +137,32 @@ extension PhotoAlbumViewController: MKMapViewDelegate {
         }
     }
 }
-//
-//// MARK: - Private
-//private extension PhotoAlbumViewController {
-//  func photo(for indexPath: IndexPath) -> FlickrPhoto {
-//    return search[indexPath.section].searchResults[indexPath.row]
-//  }
-//}
+
+// MARK: - Collection View Flow Layout Delegate
+extension PhotoAlbumViewController : UICollectionViewDelegateFlowLayout {
+  //1
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      sizeForItemAt indexPath: IndexPath) -> CGSize {
+    //2
+    let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+    let availableWidth = view.frame.width - paddingSpace
+    let widthPerItem = availableWidth / itemsPerRow
+    
+    return CGSize(width: widthPerItem, height: widthPerItem)
+  }
+  
+  //3
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      insetForSectionAt section: Int) -> UIEdgeInsets {
+    return sectionInsets
+  }
+  
+  // 4
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return sectionInsets.left
+  }
+}
