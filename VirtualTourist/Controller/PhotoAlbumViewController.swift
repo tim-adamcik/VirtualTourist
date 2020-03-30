4b415a3bffe5ebd0c38535d2818287150aa73a66
Timothy Adamcik <tadamcik0425@gmail.com>
2020-03-30 12:09:22 -0400

    modified collection view w 3 columns of images

--- VirtualTourist/Controller/PhotoAlbumViewController.swift
+++ VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -118,6 +118,20 @@ extension PhotoAlbumViewController : UICollectionViewDelegateFlowLayout, UIColle
            return 0
        }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.layer.borderColor = UIColor.blue.cgColor
+        cell?.layer.borderWidth = 1
+        cell?.isSelected = true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.layer.borderColor = UIColor.clear.cgColor
+        cell?.layer.borderWidth = 1
+        cell?.isSelected = false
+    }
+    
     
 }
 
--- VirtualTourist/View/Cell/FlickrViewCell.swift
+++ VirtualTourist/View/Cell/FlickrViewCell.swift
@@ -12,22 +12,22 @@ class FlickrViewCell: UICollectionViewCell {
     
     @IBOutlet weak var photoImage: UIImageView!
     
-    func setImageFrom(url: URL, placeholderImage: UIImage? = nil, completionHandler: @escaping (UIImage?, Error?) -> Void) {
-        
-        if let placeholderImage = placeholderImage {
-            photoImage.image = placeholderImage
-        }
-        
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data else {
-                completionHandler(nil, error)
-                return }
-            
-            let image = UIImage(data: data)
-            completionHandler(image, nil)
-        }
-        task.resume()
-    }
+//    func setImageFrom(url: URL, placeholderImage: UIImage? = nil, completionHandler: @escaping (UIImage?, Error?) -> Void) {
+//        
+//        if let placeholderImage = placeholderImage {
+//            photoImage.image = placeholderImage
+//        }
+//        
+//        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+//            guard let data = data else {
+//                completionHandler(nil, error)
+//                return }
+//            
+//            let image = UIImage(data: data)
+//            completionHandler(image, nil)
+//        }
+//        task.resume()
+//    }
     
     func setupCell(url: URL) {
         let task = URLSession.shared.dataTask(with: url) { (data, _, _) in
@@ -40,22 +40,22 @@ class FlickrViewCell: UICollectionViewCell {
         task.resume()
     }
     
-    func handleImageResponse(imageData: FlickrPhoto?, error: Error?) {
-        guard let imageMessage = URL(string: (imageData?.imageURLString())!) else {
-                   print("cannot print URL")
-                   return
-               }
-        setImageFrom(url: imageMessage, placeholderImage: #imageLiteral(resourceName: "film-reel"), completionHandler: handleImageFileResponse(image:error:))
-    }
-    
-    func handleImageFileResponse(image: UIImage?, error: Error?) {
-        DispatchQueue.main.async {
-            if let error = error {
-                print(error)
-            } else {
-                self.photoImage.image = image
-            }
-        }
-    }
-    
+//    func handleImageResponse(imageData: FlickrPhoto?, error: Error?) {
+//        guard let imageMessage = URL(string: (imageData?.imageURLString())!) else {
+//                   print("cannot print URL")
+//                   return
+//               }
+//        setImageFrom(url: imageMessage, placeholderImage: #imageLiteral(resourceName: "film-reel"), completionHandler: handleImageFileResponse(image:error:))
+//    }
+//    
+//    func handleImageFileResponse(image: UIImage?, error: Error?) {
+//        DispatchQueue.main.async {
+//            if let error = error {
+//                print(error)
+//            } else {
+//                self.photoImage.image = image
+//            }
+//        }
+//    }
+//    
 }
